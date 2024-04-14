8ea41669aab8ac894221b3d5db010e7fc187fd87
Farhan Mohammed <56759448+Nemesis-12@users.noreply.github.com>
2024-04-14 18:40:38 -0500

    Update Dependencies.lua

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -7,3 +7,8 @@ Class = require 'lib/class'
 require 'src/constants'
 require 'src/Paddle'
 require 'src/Ball'
+
+require 'src/StateMachine'
+require 'src/states/BaseState'
+require 'src/states/StartState'
+require 'src/states/PlayState'
