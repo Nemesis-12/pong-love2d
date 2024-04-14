32c0a31232516c965e53ae49b3ea93211435defb
Farhan Mohammed <56759448+Nemesis-12@users.noreply.github.com>
2024-04-14 18:40:15 -0500

    Update PlayState.lua

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -6,5 +6,31 @@ function PlayState:init()
 end
 
 function PlayState:update(dt)
-    
+    -- Check if the game is paused
+    if self.paused then
+        if love.keyboard.wasPressed('space') then
+            self.paused = false
+        else
+            return
+        end
+    elseif love.keyboard.wasPressed('space') then
+        self.paused = true
+    end
+
+    -- Update paddle position
+    self.player1:update(dt)
+
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
+end
+
+function PlayState:render()
+    -- Render pause text if paused
+    if self.paused then
+        love.graphics.setFont(gFonts['large'])
+        love.graphics.printf("PAUSED", 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, 'center')
+    else
+        self.player1:render()
+    end
 end
