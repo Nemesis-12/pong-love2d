e0d04f5d19f74d4bcd2b5ff30ce9897d7679c25c
Farhan Mohammed <56759448+Nemesis-12@users.noreply.github.com>
2024-04-14 20:52:42 -0500

    Update StateMachine.lua

--- src/StateMachine.lua
+++ src/StateMachine.lua
@@ -1,5 +1,10 @@
+-- Declare State Machine class
+-- This state machine was created based on the book 'How to Make an RPG'
+-- by Daniel Schuller. Good read especially if you want to start
+-- creating RPGs from scratch.
 StateMachine = Class{}
 
+-- Initialize State machine
 function StateMachine:init(states)
 	self.empty = {
 		render = function() end,
@@ -11,6 +16,7 @@ function StateMachine:init(states)
 	self.current = self.empty
 end
 
+-- Change current state
 function StateMachine:change(stateName, enterParams)
 	assert(self.states[stateName]) -- state must exist!
 	self.current:exit()
@@ -18,10 +24,12 @@ function StateMachine:change(stateName, enterParams)
 	self.current:enter(enterParams)
 end
 
+-- Update game for current state
 function StateMachine:update(dt)
 	self.current:update(dt)
 end
 
+-- Render objects for current state
 function StateMachine:render()
 	self.current:render()
 end
