b0300c9383b42697f9ac96075c5c419e4b493e89
Farhan Mohammed <56759448+Nemesis-12@users.noreply.github.com>
2024-04-14 20:24:48 -0500

    Update StartState.lua

--- src/states/StartState.lua
+++ src/states/StartState.lua
@@ -1,11 +1,8 @@
 StartState = Class{__includes = BaseState}
 
-highlighted = 0
-
 function StartState:update(dt)
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-        highlighted = 1
-        gStateMachine:change('play')
+        gStateMachine:change('serve')
     end
 
     if love.keyboard.wasPressed('escape') then
@@ -18,9 +15,6 @@ function StartState:render()
     love.graphics.setFont(gFonts['large'])
     love.graphics.printf("PONG", 0, VIRTUAL_HEIGHT / 3, VIRTUAL_WIDTH, 'center')
 
-    if highlighted == 1 then
-        love.graphics.setColor(1, 0, 0, 1) 
-    end
     love.graphics.setFont(gFonts['medium'])
     love.graphics.printf("Press ENTER to Start", 0, VIRTUAL_HEIGHT / 2 + 70, VIRTUAL_WIDTH, 'center')
 
