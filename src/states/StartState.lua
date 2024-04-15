da9eaed6f84026ae1cd11fcba1cec2a0e9877ec2
Farhan Mohammed <56759448+Nemesis-12@users.noreply.github.com>
2024-04-14 20:54:40 -0500

    Added comments

--- src/states/StartState.lua
+++ src/states/StartState.lua
@@ -1,17 +1,22 @@
+-- Declare Start state class
 StartState = Class{__includes = BaseState}
 
+-- Update state
 function StartState:update(dt)
+    -- Check if enter was pressed and change to serve state
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
         gStateMachine:change('serve')
     end
 
+    -- Check if player wants to quit
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
     end
 end
 
+-- Render objects
 function StartState:render()
-    -- Render text, X position, Y position, Pixels to center, Alignment mode
+    -- Render text
     love.graphics.setFont(gFonts['large'])
     love.graphics.printf("PONG", 0, VIRTUAL_HEIGHT / 3, VIRTUAL_WIDTH, 'center')
 
